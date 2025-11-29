2512fe8b623e22c1402f91bd054a4b1bad5264e5
Emil Dahlke <emil@konfidal.eu>
2025-11-29 14:26:35 +0100

    Update Config to 2025, Germany and added security levels

--- src/config.py
+++ src/config.py
@@ -1,13 +1,14 @@
+import os
 import pytz
 import logging
 
-YEAR           = 2020
-SESSION_COOKIE = ""
-LEADERBOARD    = ""
-URL            = f"https://adventofcode.com/{YEAR}/leaderboard/private/view/{LEADERBOARD}.json"
-DISCORD_TOKEN  = ''
-COMMAND_PREFIX = '!'
+YEAR = 2025
+SESSION_COOKIE = os.environ['SESSION_COOKIE']
+LEADERBOARD = os.environ['LEADERBOARD_ID']
+URL = f"https://adventofcode.com/{YEAR}/leaderboard/private/view/{LEADERBOARD}.json"
+DISCORD_TOKEN = os.environ['DISCORD_TOKEN']
+COMMAND_PREFIX = '*'
 UTC = pytz.timezone('UTC')
-EST = pytz.timezone('US/Eastern')
+EST = pytz.timezone('Europe/Berlin')
 
 logging.basicConfig(level=logging.DEBUG)
